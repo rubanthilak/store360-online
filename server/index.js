d1cf633cf36f450093c7b018432ca64e947349bd
rubanthilak <rubanthilak@zoho.com>
2021-08-25 14:49:20 +0530

    server : added login & signup routes

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const config = require("config");
+const mongoose = require("mongoose");
+const signup = require("./routes/signup");
+const login= require("./routes/login");
+const createForm = require("./routes/createForm")
+const express = require("express");
+const deleteDetail = require("./routes/deleteFile")
+const app = express();
+
+// if (!config.get("jwtPrivateKey")) {
+//   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+//   process.exit(1);
+// }
+
+mongoose.connect('mongodb://localhost/vitasoft')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use("/login", login);
+app.use("/signup", signup);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ server/routes/signup.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try{
+
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+
+
+    user = new User({
+      username:req.body.username,
+      email:req.body.email,
+      password:req.body.password
+    })
+  
+    user = new User(_.pick(req.body, ['username', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+  
+    const token = user.generateAuthToken();
+    console.log(token);
+    res.header("x-auth-token",token).send(_.pick(user, ['_id', 'username', 'email']));
+  }
+  catch(err){
+    console.log(err);
+  }
+  });
+  
+  module.exports = router; 
+  
