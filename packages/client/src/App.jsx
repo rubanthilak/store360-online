c27b1a4eba6552c4ec48a7ac4b072c38492c9a50
rubanthilak <rubanthilak@zoho.com>
2021-08-26 20:43:05 +0530

    client: testing routes

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './views/home/Home';
+import Products from './views/products/Products';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Switch>
+        <Route path="/products" >
+          <Products />
+        </Route>
+        <Route path="/" >
+          <Home />
+        </Route>
+      </Switch>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ packages/client/src/views/products/Products.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Products() {
+    return <h1>The Product Page</h1>
+}
+
+export default Products;
