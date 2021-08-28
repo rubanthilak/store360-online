1c42fa92f1f51265abfc1d8d1f1ef05644314c33
rubanthilak <rubanthilak@zoho.com>
2021-08-28 12:14:36 +0530

    client: configured redux store

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store/index";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
